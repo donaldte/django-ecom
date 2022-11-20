86b91e9a1cdf45a46a314f9e94e10672e730f0c6
donalte <donaldtedom0@gmail.com>
2022-11-20 07:23:56 +0100

    feat: personnalisation de l'espace admin

--- ECOM/shop/admin.py
+++ ECOM/shop/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
+class ClientModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email')
 
 
-admin.site.register(Client)
-admin.site.register(Produit)
-admin.site.register(Category)
-admin.site.register(Commande)
-admin.site.register(CommandeArticle)
-admin.site.register(AddressChipping)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class ProduitModelAdmin(admin.ModelAdmin):
+    list_display = ('categorie', 'name', 'price', 'digital', 'image', 'date_ajout')    
+
+class CommandeModelAdmin(admin.ModelAdmin):
+    list_display = ('client', 'complete', 'status', 'total_trans', 'transaction_id', 'date_commande')    
+
+class CommandeArticleModelAdmin(admin.ModelAdmin):
+    list_display = ('produit', 'commande', 'quantite', 'date_added')  
+
+class AddressChippingModelAdmin(admin.ModelAdmin):
+    list_display = ('client', 'commande', 'addresse', 'ville', 'zipcode', 'date_ajout')      
+
+
+admin.site.register(Client, ClientModelAdmin)
+admin.site.register(Produit, ProduitModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Commande, CommandeModelAdmin)
+admin.site.register(CommandeArticle, CommandeArticleModelAdmin)
+admin.site.register(AddressChipping, AddressChippingModelAdmin)
+
+admin.site.site_title = "Django Ecom"
+admin.site.site_header = "Django Ecom"
+admin.site.index_title = "DJango Ecom"
